fd91617734d619db6eb348f086e3d8889b01ceb6
OP-king <31962483+Duckalot-OP-king@users.noreply.github.com>
2020-11-13 23:56:58 +0100

    Added: Basic Movement and Cam.controls

---
+++ GameDevDungeonGame/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController contrl;
+
+    public float Walkspeed;
+    public float Runspeed;
+    public float JumpHeight;
+
+    private float gravity = -9.82f;
+    
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //Get input for Movement horizontal and vertical
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        contrl.Move(move * Walkspeed * Time.deltaTime);
+
+
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+        contrl.Move(velocity * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+    }
+}
